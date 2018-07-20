fdf8807f5bb6e4e10006e3ff060fa95632c6f195
joni <johnfshaughnessy@gmail.com>
2018-07-20 09:52:47 -0700

    Move parabolicCurve back into lib/ParabolicCurve.js

--- index.js
+++ index.js
@@ -1,24 +1,11 @@
 /* global THREE, AFRAME, Element  */
 var cylinderTexture = require('./lib/cylinderTexture');
 var RayCurve = require('./lib/RayCurve');
+var parabolicCurve = require("./lib/ParabolicCurve");
 
 function easeOutIn(t){
   if (t < 0.5) return 0.5 * ( (t=t*2-1) * t * t + 1);
   return 0.5*(t = t*2-1)*t*t + 0.5;
-}
-
-// Parabolic motion equation, y = p0 + v0*t + 1/2at^2
-function parabolicCurveScalar (p0, v0, halfA, t, tSquared) {
-  return p0 + v0 * t + halfA * tSquared;
-}
-
-// Parabolic motion equation applied to 3 dimensions
-function parabolicCurve (p0, v0, halfA, t, out) {
-  const tSquared = t*t;
-  out.x = parabolicCurveScalar(p0.x, v0.x, halfA.x, t, tSquared);
-  out.y = parabolicCurveScalar(p0.y, v0.y, halfA.y, t, tSquared);
-  out.z = parabolicCurveScalar(p0.z, v0.z, halfA.z, t, tSquared);
-  return out;
 }
 
 /**
